c0b644b6e5ea106a59734afb8383819f8cb47d42
GustavoBarros360 <gustavobarros2001@gmail.com>
2022-07-20 01:43:11 -0300

    fix endpoint

--- src/api.ts
+++ src/api.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 // Subir sempre com a segunda base url
 // A primeira é só pra testes locais
 // const baseURL = "http://localhost:4000";
-const baseURL = "http://projeto-lab-eng-soft.herokuapp.com/";
+const baseURL = "https://projeto-lab-eng-soft.herokuapp.com/";
 
 export const api = axios.create({ baseURL });
